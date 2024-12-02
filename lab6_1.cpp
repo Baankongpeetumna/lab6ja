cc8554e7022b115d49ec600b906f865875ab5217
Baibua <Feelgood0456@gmail.com>
2024-12-02 16:11:22 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,32 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main(){
+int main() {
+    int x;
+    int even = 0;  
+    int odd = 0;   
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;  
+
+    if (x == 0) {
+        cout << "#Even numbers = 0" << endl;
+        cout << "#Odd numbers = 0" << endl;
+    } else {
+        while (x != 0) {
+            if (x % 2 == 0) {  
+                even++;
+            } else {  
+                odd++;
+            }
+            cout << "Enter an integer: ";
+            cin >> x;  
+        }
+
+        cout << "#Even numbers = " << even << endl;
+        cout << "#Odd numbers = " << odd << endl;
+    }
+
     return 0;
 }
